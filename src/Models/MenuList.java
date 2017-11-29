39bd99b37d66555424f722ef23edbd522ad8becc
Unknown <drdgvhbh@gmail.com>
2017-11-29 02:15:50 -0500

    derp

--- src/Models/MenuList.java
+++ src/Models/MenuList.java
@@ -1,4 +1,10 @@
 package Models;
 
+import java.util.HashMap;
+
 public class MenuList {
+    HashMap<String, Double> menuList;
+    public MenuList() {
+        menuList = new HashMap<>();
+    }
 }
